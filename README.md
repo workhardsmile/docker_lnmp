0f017c019840e4c0c83bbad43fb6a191a34fcf46
Frank Wu <gang.wu@istuary.com>
2017-04-28 16:43:57 +0800

    init publish branch.

--- README.md
+++ README.md
@@ -1,49 +1,43 @@
-> 更新container工程
-
-```
-# 第一次
-git clone git@10.0.10.120:OnlineTestSystem/covision-container.git
-# 以后每次更新
-cd covision-container 
-git pull
-```
-
-1、启动容器和更新代码（常用）
+初始化：
 ---------------------------
 ```
-# 重启container
-docker-compose stop
-docker-compose up php_nginx -d
-# 更新代码并重启queue（每次更新covision项目）
+git clone git@10.0.10.120:OnlineTestSystem/covision-container.git
+cd covision-container
+#$1:是否重启container, $2:是否执行init_es，$3:是否执行db_seed
+sh update.sh 1 1 1
+#设置域名，参照./php_nginx/project_conf/nginx-sites.conf
+docker exec -i --user www-data php_nginx sed -i "s/[a-z.]*\.covision\.com/www.covision.com/g" .env
+```
+后面更新：
+---------------------------
+```
+# 更新代码并执行必要命令
+cd covision-container 
 sh update.sh
 ```
 > 项目路径： ./data/php_nginx/www/covision
 
-2、清除Container进程
+其他命令：
 ---------------------------
+####1. 清除Container进程
 ```
 docker-compose stop 
 docker stop $(docker ps -a -q)|xargs dcoker rm
 ```
-3、本地构建镜像
----------------------------
+####2. 本地构建镜像
 ```
 # 全部构建
 docker-compose build -f docker-compose-registry-source.yml
 # 选择构建
 docker-compose build -f docker-compose-registry-source.yml php_nginx
 ```
-4、本地镜像启动
----------------------------
+####3.本地镜像启动
 ```
 docker-compose up -f docker-compose-registry-source.yml -d
 docker-compose up -f docker-compose-registry-source.yml -d php_nginx
 ```
-5、执行一次
----------------------------
+####4. 其他命令
 ```
-#本地.env可以不修改APP_URL
-cp -f ./php_nginx/project_conf/env.example ./data/php_nginx/www/covision/.env
 docker exec -i --user www-data php_nginx composer install
 docker exec -i --user www-data php_nginx php artisan migrate:install
 docker exec -i --user www-data php_nginx php artisan migrate
--- update.sh
+++ update.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
-git pull
-[ ! -d "./data/php_nginx/www/covision" ] && git clone "git@10.0.10.120:OnlineTestSystem/covision.git" ./data/php_nginx/www/covision
-cd ./data/php_nginx/www/covision
-#git pull
-#git checkout develop
-cp -f ./php_nginx/project_conf/env.example ./data/php_nginx/www/covision/.env
-#docker exec -i php_nginx chmod -R 777 storage bootstrap/cache storage/framework/views
-#docker exec -i php_nginx chown -R www-data:www-data /data/web/www/covision
-docker exec -i --user www-data php_nginx git pull
-docker exec -i --user www-data php_nginx git checkout develop
-docker exec -i --user www-data php_nginx php artisan key:generate
-docker exec -i --user www-data php_nginx php artisan migrate:install
-docker exec -i --user www-data php_nginx sh init.sh
-#kill -9 $(ps -ef|grep 'queue:work'|grep -v grep|awk '{print $2}')
-#docker exec -i --user www-data php_nginx php artisan queue:work --queue=emails &
+tag=develop
+restart_docker=false
+init_es=false
+db_seed=false
+[ -n "$1" ] && restart_docker=$1
+[ -n "$2" ] && init_es=$2
+[ -n "$3" ] && db_seed=$3
+[ ! -d "../data/php_nginx/www/covision" ] && git clone "git@10.0.10.120:OnlineTestSystem/covision.git" ../data/php_nginx/www/covision
+#mkdir -p ../data/php_nginx/www/covision && cp -fr ./data/php_nginx/www/covision ../data/php_nginx/www/
+mkdir -p ../data/elasticsearch/config && cp -fr ./elasticsearch/config/* ../data/elasticsearch/config/
+mkdir -p ../data/php_nginx/conf && cp -f ./php_nginx/project_conf/nginx-sites.conf ../data/php_nginx/conf/
+mkdir -p ../data/php_nginx/conf && cp -f ./php_nginx/project_conf/supervisor.conf ../data/php_nginx/conf/
+if [ "$restart_docker" != "false" ]; then
+  docker-compose stop
+  docker stop $(docker ps -a -q)|xargs docker rm ; docker rmi $(docker images | grep "^<none>" | awk '{print $3}') -f
+  docker-compose up -d php_nginx
+fi
+registration_name="php_nginx" #"php-nginx-alpine"
+container_id=0
+while [ "$container_id" = "0" ]
+ do
+    container_id=$(docker ps|grep "$registration_name"|awk '{print $1}')
+    [ "$container_id" = "" ] && container_id=0
+ done
+echo $container_id
+if [ "$init_es" != "false" ]; then
+  cp -f ./php_nginx/project_conf/env.example ./../data/php_nginx/www/covision/.env
+  #docker exec -i $container_id chmod -R 777 storage bootstrap/cache storage/framework/views
+  #docker exec -i $container_id chown -R www-data:www-data /data/web/www/covision
+  docker exec -i --user www-data $container_id composer install
+  docker exec -i --user www-data $container_id php artisan migrate:install
+  docker exec -i --user www-data $container_id php artisan key:generate
+  docker exec -i --user www-data $container_id killall -9 php
+fi
+docker exec -i --user www-data $container_id git checkout $tag
+docker exec -i --user www-data $container_id git pull
+docker exec -i --user www-data $container_id sh init.sh
+if [ "$init_es" != "false" ]; then
+  docker exec -i --user www-data $container_id php artisan init:es
+fi
+if [ "$db_seed" != "false" ]; then
+  docker exec -i --user www-data $container_id php artisan db:seed
+fi
